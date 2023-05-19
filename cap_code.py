b50ca13c7978172880c550d2038c844ae1ac044c
SDGaming317 <134094466+SDGaming317@users.noreply.github.com>
2023-05-19 14:49:39 -0400

    Add files via upload

---
+++ cap_code.py
@@ -0,0 +1,30 @@
+schools = ["FAU", "Liberty", "Palm Beach Atlantic", "Florida Polytechnic"]
+activities = ["Work", "Move", "Strengthen faith", "Become independent", "Begin dating"]
+
+def acceptance():
+    print("Acceptances processing...\n")
+    for school in schools:
+        print("Accepted into " + school)
+    print("\nAcceptance process completed \n")
+
+def elim():
+    print("Deciding which schools...\n")
+    for school in range(0, len(schools)-1):
+        print("Graciously decline " + schools[school])
+    print("\nFinal decision: Florida Poly\n")
+
+def main(yr):
+    if  yr >= 12:
+        # Begin Applications
+        print("Beginning application process...\n...")
+        for school in schools:
+            print("Applying to " + school)
+        print()
+        # Receive Acceptance letters
+        acceptance()
+        elim()
+    else:
+        print("Continue until senior year")
+
+yr = input("What year in high-school are you? \n")
+main(int(yr))
